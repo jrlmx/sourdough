a52ce0ae8a7d2bd68cd69a84f0fe0baa71405170
JRLmx <bocaj.xml@gmail.com>
2025-01-02 14:52:48 -0500

    added pages/index.blade.php

--- stubs/routes/web.php
+++ stubs/routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('welcome');
-
 Route::middleware('auth')->group(function (): void {
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
